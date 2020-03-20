455fe539e39880b06146e954f3962f0b67e1af97
Aguilera Lorenzo <aguilera.lorenzo@gmail.com>
2020-03-20 16:52:16 +0100

    modify script platform

--- Assets/PlatformScript.cs
+++ Assets/PlatformScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
+    void OnCollisionEnter2D(Collision2D collision){
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
