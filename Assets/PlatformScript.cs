56db96eba6d347977f119639e57fd413f2b74913
Aguilera Lorenzo <aguilera.lorenzo@gmail.com>
2020-03-25 18:34:38 +0100

    add script

--- Assets/PlatformScript.cs
+++ Assets/PlatformScript.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    public float jumpForce = 4f;
+    public float jumpForce = 50f;
         void OnCollisionEnter2D(Collision2D collision){
+
+            if(collision.relativeVelocity.y<= 0f){
         Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
         if(rb!=null){
             Vector2 velocity = rb.velocity;
             velocity.y =jumpForce;
             rb.velocity = velocity;
-        }
+        }}
     }
     // Start is called before the first frame update
     void Start()
